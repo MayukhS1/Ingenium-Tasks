913c719617816953aabd71d0a02436253ca87877
Mayukh Sasmal <49561618+MayukhS1@users.noreply.github.com>
2021-05-21 20:39:59 +0530

    project-folder-added

---
+++ svg-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ReactiveSVG from './ResponsiveSVGtest1';
+
+function App() {
+  return (
+    <React.Fragment>
+      <ReactiveSVG/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ svg-app/src/components/ResponsiveSVGtest1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ResponsiveSVGtest1 (){
+    
+};
+
+export default ResponsiveSVGtest1;
