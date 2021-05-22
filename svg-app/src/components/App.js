49f21b214c1a6a5e2c6e8fc3bedd95752ef72066
Mayukh Sasmal <mayukhsasmal16@gmail.com>
2021-05-22 20:07:20 +0530

    Base-done

--- svg-app/src/components/App.js
+++ svg-app/src/components/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import './App.css';
-import Component2 from './Componemt2';
-import ResponsiveSVGtest1 from './ResponsiveSVGtest1';
+import React, { useState } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Header from './contents/Header';
+import Base from './contents/Base';
+import Home from './contents/Home';
+import Toppings from './contents/Toppings';
+import Order from './contents/Order';
 
 function App() {
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
+
+  const addBase = (base) => {
+    setPizza({ ...pizza, base })
+  }
+  
+  const addTopping = (topping) => {
+    let newToppings;
+    if(!pizza.toppings.includes(topping)){
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter(item => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  }
+
   return (
-    <React.Fragment>
-      <ResponsiveSVGtest1/>
-      <Component2/>
-    </React.Fragment>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/base">
+          <Base addBase={addBase} pizza={pizza} />
+        </Route>
+        <Route path="/toppings">
+          <Toppings addTopping={addTopping} pizza={pizza} />
+        </Route>
+        <Route path="/order">
+          <Order pizza={pizza} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
