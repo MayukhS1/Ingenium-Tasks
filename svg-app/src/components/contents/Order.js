7cc151f8c064401b27cdf62745a96ab7234f5589
Mayukh Sasmal <mayukhsasmal16@gmail.com>
2021-05-30 20:58:12 +0530

    remove-elemen-from-render-with-animate-presence

--- svg-app/src/components/contents/Order.js
+++ svg-app/src/components/contents/Order.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {motion} from 'framer-motion';
+import React, {useState} from 'react';
+import {motion, AnimatePresence} from 'framer-motion';
 
 const containerVarients ={
     hidden:{
@@ -32,13 +32,24 @@ const childVarient={
 };
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true);
+  setTimeout(()=>{
+    setShowTitle(false);
+  },4000);
+
   return (
     <motion.div className="container order"
     variants={containerVarients}
     initial="hidden"
     animate="visible"
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+                <motion.h2
+                  exit={{ y:-1000 }}
+                >Thank you for your order :)</motion.h2>
+        )}
+      </AnimatePresence>
       <motion.p
         variants={childVarient}
       >You ordered a {pizza.base} pizza with:</motion.p>
