361c0b7dab74f58a439c572963c766a8d677f1ae
Mayukh Sasmal <mayukhsasmal16@gmail.com>
2021-05-31 00:10:40 +0530

    basic-svg-animation

--- svg-app/src/components/contents/Header.js
+++ svg-app/src/components/contents/Header.js
@@ -1,19 +1,52 @@
 import React from 'react';
 import {motion} from 'framer-motion';
+
+
+const svgVariant ={
+  start : {rotate: -180},
+  end : {
+    rotate : 0,
+    transition:{
+      duration: 1
+    }
+  },
+};
+
+const pathVariants ={
+  start: {
+    opacity: 0,
+    pathLength: 0
+  },
+  end:{
+    opacity: 1,
+    pathLength : 1,
+    transition :{
+      duration : 2,
+      ease : "easeInOut"
+    }
+  }
+}
+
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
+        <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
+          variants = {svgVariant}
+          initial="start"
+          animate="end"
+        >
+          <motion.path
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVariants}
           />
-          <path
+          <motion.path
             fill="none"
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            variants={pathVariants}
           />
-        </svg>
+        </motion.svg>
       </div>
       <motion.div className="title"
         initial={{y: -200}}
